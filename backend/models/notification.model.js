c00a986dae3b6dbb9cfefbfcdff1376098e5a191
AhmedYtawfik <ahmedyasser55577@gmail.com>
2024-09-28 12:57:56 +0300

    fix notificatioins pleaaaaaaaaaaaase

--- backend/models/notification.model.js
+++ backend/models/notification.model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-
+// who has notifications? :  admin, tourist, seller, advertiser
 const notificationSchema = new mongoose.Schema(
   {
-    tourist: { type: mongoose.Schema.Types.ObjectId, ref: "Tourist" },
+    tourtoist: { type: mongoose.Schema.Types.ObjectId, ref: "Tourist" },
     actionType: String,
     relatedId: { type: mongoose.Schema.Types.ObjectId, required: true },
     message: { type: String, required: true },
