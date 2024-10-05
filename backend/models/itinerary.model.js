e32dd31d2559ae189983f2f92cef2e48f086ce9a
arwakhattab <arwa5attab@gmail.com>
2024-10-05 13:54:33 +0300

    undid changes to itinerary schema

--- backend/models/itinerary.model.js
+++ backend/models/itinerary.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { validateReference, validateReferences } from "./validatingUtils.js";
 
 // total price is price of existing activities + landmark ticketPrices
 
@@ -10,24 +9,22 @@ const itinerarySchema = new mongoose.Schema(
       ref: "TourGuide",
       required: true,
     },
-    activities: { type: [
-        {
-          activityType: {
-            type: String,
-            enum: ["Activity", "CustomActivity"],
-            required: true,
-          },
-          activity: {
-            type: mongoose.Schema.Types.ObjectId,
-            refPath: "activityType",
-            required: true,
-          },
-          startTime: Date, // to be upadated
-          endTime: Date, // to be updated
+    activities: [
+      {
+        activityType: {
+          type: String,
+          enum: ["Activity", "CustomActivity"],
+          required: true,
         },
-      ],
-      default: []
-    },
+        activity: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: "activityType",
+          required: true,
+        },
+        startTime: Date, // to be upadated
+        endTime: Date, // to be updated
+      },
+    ],
     language: String,
     accessibility: [String],
     price: { type: Number, required: true },
@@ -54,99 +51,6 @@ const itinerarySchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
-// tourguide, tags, rating, activities;
-
-const validateTourguideID = async (tourguideID, next) => {
-  try {
-    if (!tourguideID) {
-      return next(new Error("tourguideID is required."));
-    }
-    await validateReference(tourguideID, "TourGuide", next);
-  } catch (err) {
-    return next(err);
-  }
-};
-
-const validateActivities = async (activities, next) => {
-  try {
-    const validModels = ["Activity", "CustomActivity"];
-    for (const activityObj of activities) {
-      const { activityType, activity } = activityObj;
-      if (!validModels.includes(activityType)) {
-        return next(new Error(`Invalid activityType: ${activityType}`));
-      }
-      await validateReference(activity, activityType, next);
-    }
-  } catch (err) {
-    return next(err);
-  }
-};
-
-const validateTags = async (tags, next) => {
-  try {
-    if (tags && tags.length) {
-      await validateReferences(tags, "Tag", next);
-    }
-  } catch (err) {
-    return next(err);
-  }
-};
-
-const validateRatings = async (ratings, next) => {
-  try {
-    if (ratings && ratings.length) {
-      await validateReferences(ratings, "Rating", next);
-    }
-  } catch (err) {
-    return next(err);
-  }
-};
-
-itinerarySchema.pre("save", async function (next) {
-  try {
-    const { tourguideID, activities, tags, ratings } = this;
-
-    await validateTourguideID(tourguideID, next);
-    await validateActivities(activities, next);
-    await validateTags(tags, next);
-    await validateRatings(ratings, next);
-  } catch (error) {
-    next(error);
-  }
-});
-
-const validateUpdateItinerary = async function (next) {
-  try {
-    const update = this.getUpdate();
-
-    const tourguideID = update.tourguideID || update["$set.tourguideID"];
-    const activities = update.activities || update["$set.activities"];
-    const tags = update.tags || update["$set.tags"];
-    const ratings = update.ratings || update["$set.ratings"];
-
-    if (tourguideID) {
-      await validateTourguideID(tourguideID, next);
-    }
-    if (activities) {
-      await validateActivities(activities, next);
-    }
-    if (tags) {
-      await validateTags(tags, next);
-    }
-    if (ratings) {
-      await validateRatings(ratings, next);
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
-
-itinerarySchema.pre("findOneAndUpdate", validateUpdateItinerary);
-itinerarySchema.pre("updateOne", validateUpdateItinerary);
-itinerarySchema.pre("findByIdAndUpdate", validateUpdateItinerary);
-
 
 itinerarySchema.index({ tourguideID: 1 });
 
