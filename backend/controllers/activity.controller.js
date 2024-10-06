60593b5656f981f9888c3dc67dc41af796e98853
arwakhattab <arwa5attab@gmail.com>
2024-10-06 03:04:55 +0300

    added populate method in getUpcomingActivities

--- backend/controllers/activity.controller.js
+++ backend/controllers/activity.controller.js
@@ -1,13 +1,16 @@
 import Activity from "../models/activity.model.js";
 
 // view upcoming activities that are open for booking and are not flagged
-export const getUpcomingActivities = async (req, res) => { 
-  
+export const getUpcomingActivities = async (req, res) => {
   try {
-    const activities = await Activity.find({isOpenForBooking: true, isFlagged: false, startDate: {$gt: Date.now()}});
+    const activities = await Activity.find({
+      isFlagged: false, // activities that are flagged do not appear to the user according to requirement (33)
+      startDate: { $gt: Date.now() },
+    })
+      .populate("advertiserID")
+      .populate("ratings");
     res.json(activities);
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
