36a2231a72cf625ec8ada303f79b1b0e9c26ea0e
AhmedYtawfik <ahmedyasser55577@gmail.com>
2024-10-06 19:33:38 +0300

    requirment 4 , 5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,24 @@ import CreateProductPage from "./pages/Product/CreateProductPage";
 import UpdateProductPage from "./pages/Product/UpdateProductPage";
 import TourGuideProfilePage from "./pages/TourGuide/TourGuideProfilePage";
 import NavBar from "./components/NavBar";
-
+import SignupPage from "./pages/TouristSup/SignupPage";
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/create-product" element={<CreateProductPage />} />
-                <Route
-                    path="/update-product/:productId"
-                    element={<UpdateProductPage />}
-                />
-                <Route path="/tourguide" element={<TourGuideProfilePage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/create-product" element={<CreateProductPage />} />
+        <Route
+          path="/update-product/:productId"
+          element={<UpdateProductPage />}
+        />
+        <Route path="/tourguide" element={<TourGuideProfilePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
