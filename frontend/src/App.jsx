56a20390631cbb85879925668db7d44d82c5604f
roba <robahesham25@gmail.com>
2024-10-06 23:37:56 +0300

    added listOfObjects component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/HomePage";
@@ -7,24 +7,32 @@ import AboutPage from "./pages/AboutPage";
 import CreateProductPage from "./pages/Product/CreateProductPage";
 import UpdateProductPage from "./pages/Product/UpdateProductPage";
 import TourGuideProfilePage from "./pages/TourGuide/TourGuideProfilePage";
+import AdvertiserProfilePage from "./pages/Advertiser/AdvertiserProfilePage";
+import CreateActivityPage from "./pages/Activity/CreateActivityPage";
+import UpdateActivityPage from "./pages/Activity/UpdateActivityPage";
+import FilterLandmarks from "./pages/Landmarks/FilterLandmarks";
 import NavBar from "./components/NavBar";
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/create-product" element={<CreateProductPage />} />
-                <Route
-                    path="/update-product/:productId"
-                    element={<UpdateProductPage />}
-                />
-                <Route path="/tourguide" element={<TourGuideProfilePage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/create-product" element={<CreateProductPage />} />
+        <Route
+          path="/update-product/:productId"
+          element={<UpdateProductPage />}
+        />
+        <Route path="/tourguide" element={<TourGuideProfilePage />} />
+        <Route path="/advertiser" element={<AdvertiserProfilePage />} />
+        <Route path="/create-activity" element={<CreateActivityPage />} />
+        <Route path="/update-activity" element={<UpdateActivityPage />} />
+        <Route path="/filter-landmarks" element={<FilterLandmarks />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
