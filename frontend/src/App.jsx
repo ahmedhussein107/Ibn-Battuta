8ba1e358a3edcc6020e9f31afd6574f8ac41a005
arwakhattab <arwa5attab@gmail.com>
2024-10-07 06:41:34 +0300

    finished the read and update part of requirement 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,26 @@ import AboutPage from "./pages/AboutPage";
 import CreateProductPage from "./pages/Product/CreateProductPage";
 import UpdateProductPage from "./pages/Product/UpdateProductPage";
 import TourGuideProfilePage from "./pages/TourGuide/TourGuideProfilePage";
+import SellerProfilePage from "./pages/Seller/SellerProfilePage";
 import NavBar from "./components/NavBar";
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/create-product" element={<CreateProductPage />} />
-                <Route
-                    path="/update-product/:productId"
-                    element={<UpdateProductPage />}
-                />
-                <Route path="/tourguide" element={<TourGuideProfilePage />} />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/create-product" element={<CreateProductPage />} />
+				<Route
+					path="/update-product/:productId"
+					element={<UpdateProductPage />}
+				/>
+				<Route path="/tourguide" element={<TourGuideProfilePage />} />
+				<Route path="/seller" element={<SellerProfilePage />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const URL = import.meta.env.VITE_API_URL;
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+const URI = import.meta.env.VITE_API_URI;
+console.log("VITE_API_URL:", URI);
 
 const axiosInstance = axios.create({
-  baseURL: `${URL}api/`,
-  timeout: 100000, // more than enought ,100 secs
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: `${URI}api/`,
+	timeout: 100000, // more than enought ,100 secs
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 console.log(URL);
