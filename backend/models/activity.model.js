f7e91ec35e5bff6352681f97d9549093d1c84e27
roba <robahesham25@gmail.com>
2024-11-05 10:48:25 +0200

    added title,description and picture in activity  model

--- backend/models/activity.model.js
+++ backend/models/activity.model.js
@@ -11,6 +11,8 @@ const activitySchema = new Schema(
         },
         Latitude: { type: Number },
         Longitude: { type: Number },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
         startDate: { type: Date, required: true },
         endDate: { type: Date, required: true },
         price: Number,
@@ -22,6 +24,7 @@ const activitySchema = new Schema(
         sumOfRatings: { type: Number, default: 0 },
         freeSpots: { type: Number, required: true },
         specialDiscount: { type: Number, default: 0 },
+        picture: String,
     },
     { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
