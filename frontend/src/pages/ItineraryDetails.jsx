9016daba9519b4089ef8bc9eae62c3ed41fe7311
raffayyl@gmail.com <raffayyl@gmail.com>
2024-11-05 14:26:33 +0200

    Add available dates and times components

--- frontend/src/pages/ItineraryDetails.jsx
+++ frontend/src/pages/ItineraryDetails.jsx
@@ -1,3 +1,4 @@
+import AvailableDates from "../components/ItineraryDetails/AvailableDates";
 import Book from "../components/ItineraryDetails/Book";
 
 export default function ItineraryDetails() {
@@ -8,11 +9,7 @@ export default function ItineraryDetails() {
 	return (
 		<div>
 			<h1>Itinerary Details</h1>
-			<Book
-				price={"$99.00"}
-				text={"Likely sell out"}
-				onClick={handleBooking}
-			/>
+			<AvailableDates width={"30%"} height={"50%"} />
 		</div>
 	);
 }
