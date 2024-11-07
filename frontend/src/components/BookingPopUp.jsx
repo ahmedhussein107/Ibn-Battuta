a85f27e0fe2b13779ab6e4270fe59d30608d0869
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-11-07 11:28:36 +0200

    added success and error popup and backend for booking

--- frontend/src/components/BookingPopUp.jsx
+++ frontend/src/components/BookingPopUp.jsx
@@ -6,99 +6,91 @@ import axiosInstance from "../api/axiosInstance";
 import { useState } from "react";
 import PopUp from "./PopUpsGeneric/PopUp";
 import PopUpSuccess from "./PopUpsGeneric/PopUpSuccess";
-//count
-//summary
-const BookingPopUp = ({ isOpen, setIsOpen }) => {
-    const [numberOfTickets, setNumberOfTickets] = useState(1);
+import PopUpError from "./PopUpsGeneric/PopUpError";
 
-    const itinerary = {
-        _id: "6703f5310ecc1ad25ff95144",
-        name: "tour in GUC",
-        description:
-            "best itinerary ever as ,I also love ahmed hussien because he is my friend",
-        tourguideID: "6700044e887e126c909d6f21",
-        activities: [
-            {
-                activityType: "Activity",
-                activity: "670405f81ddb4f53fd971cd8",
-                _id: "6703f5310ecc1ad25ff95145",
-                id: "6703f5310ecc1ad25ff95145",
-            },
-        ],
-        language: "Arabic",
-        accessibility: ["weelchair", "ambulance cars"],
-        price: 1000,
-        availableDatesAndTimes: ["2024-12-20T00:00:00.000Z"],
-        pickup: "GUC",
-        dropOff: "GUC",
-        tags: ["sky diving", "sea"],
-        isActivated: true,
-        isFlagged: false,
-        ratings: ["672b666a8c7e37c372c27ebd"],
-        sumOfRatings: 5,
-        createdAt: "2024-10-07T14:50:25.807Z",
-        updatedAt: "2024-11-06T12:51:54.972Z",
-        location: "Cairo,Eg",
-        picture: "https://i.postimg.cc/dtYPjDgS/guc.jpg",
-        __v: 1,
-        rating: 5,
-        id: "6703f5310ecc1ad25ff95144",
-    };
+const BookingPopUp = ({
+    isOpen,
+    setIsOpen,
+    BookingType = "Itinerary",
+    toBeBookedItem = { _id: "6703f5310ecc1ad25ff95144", price: 2132 },
+    touristID = "670442014aa7c398b29183c9",
+}) => {
+    const [numberOfTickets, setNumberOfTickets] = useState(1);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const handleSubmit = async () => {
         try {
             //buy ticket;
-
+            await axiosInstance.post("/booking/createBooking", {
+                touristID,
+                bookingType: BookingType,
+                typeId: toBeBookedItem._id,
+                count: numberOfTickets,
+                totalPrice: numberOfTickets * toBeBookedItem.price,
+            });
             setIsOpen(false);
-            window.location.reload();
+            setIsSuccess(true);
         } catch (err) {}
     };
 
     return (
-        <PopUp
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-            headerText={"Please fill in the following to complete your booking"}
-            actionText={"Confirm"}
-            handleSubmit={handleSubmit}
-        >
-            <div className="ticket-container">
-                <div className="ticket-counter">
-                    <button
-                        className="counter-btn"
-                        disabled={numberOfTickets === 1}
-                        onClick={() => setNumberOfTickets(numberOfTickets - 1)}
-                    >
-                        -
-                    </button>
-                    <span id="ticket-count">{numberOfTickets}</span>
-                    <button
-                        className="counter-btn"
-                        onClick={() => setNumberOfTickets(numberOfTickets + 1)}
-                    >
-                        +
-                    </button>
+        <>
+            <PopUp
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+                headerText={"Please fill in the following to complete your booking"}
+                actionText={"Confirm"}
+                handleSubmit={handleSubmit}
+            >
+                <div className="ticket-container">
+                    <div className="ticket-counter">
+                        <button
+                            className="counter-btn"
+                            disabled={numberOfTickets === 1}
+                            onClick={() => setNumberOfTickets(numberOfTickets - 1)}
+                        >
+                            -
+                        </button>
+                        <span id="ticket-count">{numberOfTickets}</span>
+                        <button
+                            className="counter-btn"
+                            onClick={() => setNumberOfTickets(numberOfTickets + 1)}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className="price-info">
+                        <p>
+                            Price per person:{" "}
+                            <span className="price-per-person">
+                                $ {toBeBookedItem.price}
+                            </span>
+                        </p>
+                        <p>
+                            Total price:{" "}
+                            <span id="total-price">
+                                $ {toBeBookedItem.price * numberOfTickets}
+                            </span>
+                        </p>
+                    </div>
                 </div>
-                <div className="price-info">
-                    <p>
-                        Price per person:{" "}
-                        <span className="price-per-person">$ {itinerary.price}</span>
-                    </p>
-                    <p>
-                        Total price:{" "}
-                        <span id="total-price">
-                            $ {itinerary.price * numberOfTickets}
-                        </span>
-                    </p>
-                </div>
-            </div>
-        </PopUp>
-        // <PopUpSuccess
-        //     isOpen={isOpen}
-        //     setIsOpen={setIsOpen}
-        //     headerText={"Booking completed successfully"}
-        //     bodyText={"You earned 1000 points"}
-        // />
+            </PopUp>
+            <PopUpSuccess
+                isOpen={isSuccess}
+                setIsOpen={setIsSuccess}
+                headerText={"Booking completed successfully"}
+                bodyText={`You earned ${Math.floor(
+                    toBeBookedItem.price * numberOfTickets
+                )} points`}
+            />
+            <PopUpError
+                isOpen={isError}
+                setIsOpen={setIsError}
+                headerText={"An error occured while processing your request"}
+                bodyText={"Please try again later"}
+            />
+        </>
     );
 };
 
