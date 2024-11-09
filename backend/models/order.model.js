66e179c7f24ce3c831df5b935a58d513859cdf8b
AhmedKamal18 <ahmedelbakly95@gmail.com>
2024-11-09 19:41:00 +0200

    fix merge error

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -21,25 +21,6 @@ const orderSchema = new mongoose.Schema(
 		ratingID: { type: mongoose.Schema.Types.ObjectId, ref: "Rating" },
 	},
 	{ timestamps: true }
-    {
-        buyer: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Tourist",
-            required: true,
-        },
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: true,
-        },
-        price: { type: Number, required: true },
-        count: { type: Number, required: true },
-        address: String,
-        method: { type: String, enum: ["card", "wallet", "cash on delivery"] },
-        status: { type: String, enum: ["delivered", "canceled", "pending"] },
-        ratingID: { type: mongoose.Schema.Types.ObjectId, ref: "Rating" },
-    },
-    { timestamps: true }
 );
 
 orderSchema.pre("save", async function (next) {
