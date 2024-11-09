391df2ab866bc8dcaeb26ea905ea957221a91e51
arwakhattab <arwa5attab@gmail.com>
2024-11-09 15:31:56 +0200

    added card component for flights

---
+++ frontend/src/components/Flights/FlightCard.jsx
@@ -0,0 +1,54 @@
+import Button from "../Button";
+import TripDetails from "./TripDetails";
+
+const FlightCard = ({ trip, airlines }) => {
+    const styles = {
+        card: {
+            width: "45vw",
+            height: "25vh",
+            margin: "2vh 2vw 2vh 2vw",
+            display: "flex",
+            flexDirection: "row",
+            boxShadow: "0 2px 12px rgba(0,0,0,0.08)",
+            borderRadius: "2vh",
+        },
+        cardLeft: {
+            width: "80%",
+            //backgroundColor: "#f0f0f0",
+            borderRadius: "2vh 0 0 2vh",
+            display: "flex",
+            flexDirection: "column", // Add this
+            alignItems: "center", // Center horizontally
+            justifyContent: "center",
+            gap: "1vh",
+        },
+        cardRight: {
+            width: "20%",
+            borderColor: "rgb(127,125,125)",
+            borderStyle: "solid",
+            borderWidth: "0 0 0 0.1vw",
+            display: "flex",
+            flexDirection: "column", // Add this
+            alignItems: "center", // Center horizontally
+            justifyContent: "center",
+            gap: "1vh",
+        },
+    };
+
+    return (
+        <div style={styles.card}>
+            <div style={styles.cardLeft}>
+                {trip.itineraries.map((flight, index) => (
+                    <TripDetails flight={flight} airlines={airlines} />
+                ))}
+            </div>
+
+            <div style={styles.cardRight}>
+                <h4>{`${trip.price.total} ${trip.price.currency}`}</h4>
+                <Button stylingMode="1" text="View"></Button>
+            </div>
+        </div>
+    );
+};
+
+export default FlightCard;
