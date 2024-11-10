d315b34ec9f7dad7bfb7bba9c7be78d83f33c950
arwakhattab <arwa5attab@gmail.com>
2024-11-10 20:50:59 +0200

    edited flight booking logic

--- backend/services/flights.js
+++ backend/services/flights.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Amadeus from "amadeus";
+import { isAuthenticated } from "../routers.middleware/authentication.js";
+import Tourist from "../models/tourist.model.js";
 
 import { API_KEY, API_SECRET } from "../config/config.js";
 
@@ -54,18 +56,97 @@ amadeusFlightsRouter.get("/get-pricing", async (req, res) => {
 
 // TODO: add booking to tourist
 
-amadeusFlightsRouter.get("/book", async (req, res) => {
-    const { flightOffers, travelersList } = req.body;
+const adultTraveller = {
+    id: "1",
+    dateOfBirth: "1982-01-16",
+    name: {
+        firstName: "JORGE",
+        lastName: "GONZALES",
+    },
+    gender: "MALE",
+    contact: {
+        emailAddress: "jorge.gonzales833@telefonica.es",
+        phones: [
+            {
+                deviceType: "MOBILE",
+                countryCallingCode: "34",
+                number: "480080076",
+            },
+        ],
+    },
+    documents: [
+        {
+            documentType: "PASSPORT",
+            birthPlace: "Madrid",
+            issuanceLocation: "Madrid",
+            issuanceDate: "2015-04-14",
+            number: "00000000",
+            expiryDate: "2025-04-14",
+            issuanceCountry: "ES",
+            validityCountry: "ES",
+            nationality: "ES",
+            holder: true,
+        },
+    ],
+};
+
+const childTraveller = {
+    id: "2",
+    dateOfBirth: "2020-10-11",
+    gender: "FEMALE",
+    contact: {
+        emailAddress: "jorge.gonzales833@telefonica.es",
+        phones: [
+            {
+                deviceType: "MOBILE",
+                countryCallingCode: "34",
+                number: "480080076",
+            },
+        ],
+    },
+    name: {
+        firstName: "ADRIANA",
+        lastName: "GONZALES",
+    },
+};
+
+amadeusFlightsRouter.post("/book", isAuthenticated, async (req, res) => {
+    const touristID = req.user.userId;
+    const { flightOffer } = req.body;
+    const travelers = flightOffer.travelerPricings.map((element, index) => {
+        if (element.travelerType === "ADULT") {
+            const adult = { ...adultTraveller };
+            adult.id = element.travelerId;
+            return adult;
+        } else {
+            const child = { ...childTraveller };
+            child.id = element.travelerId;
+            return child;
+        }
+    });
+
     try {
         const bookingResponse = await amadeus.booking.flightOrders.post({
             data: {
                 type: "flight-order",
-                flightOffers,
-                travelers: travelersList,
+                flightOffers: [flightOffer],
+                travelers,
             },
         });
+
+        const tourist = await Tourist.findById(touristID);
+
+        if (tourist) {
+            if (!tourist.flightBookings) tourist.flightBookings = [];
+            tourist.flightBookings.push(bookingResponse.data);
+            await tourist.save();
+        } else {
+            return res.status(400).send({ error: "Tourist not found" });
+        }
+
         res.status(200).send({ bookingResponse });
     } catch (error) {
+        console.log(error);
         res.status(400).send({ error: error.message });
     }
 });
