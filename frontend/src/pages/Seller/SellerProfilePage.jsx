2dfd15e19a8bb63f041312fdbd64a37df58204c3
roba <robahesham25@gmail.com>
2024-11-10 14:35:23 +0200

    finished seller profile,but there is a problem with the photo

--- frontend/src/pages/Seller/SellerProfilePage.jsx
+++ frontend/src/pages/Seller/SellerProfilePage.jsx
@@ -1,15 +1,37 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 import axiosInstance from "../../api/axiosInstance";
 import Navbar from "../../components/NavBar";
 import Footer from "../../components/Footer";
-import ProfileButton from "../../components/ProfileButtons";
 import bg from "../../assets/images/bg.jpg";
+import ProfileButton from "../../components/ProfileButtons";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import PopUp from "../../components/PopUpsGeneric/PopUp";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import axios from "axios";
 
 const SellerProfilePage = () => {
     const [response, setResponse] = useState(null);
+    const [userType, setUserType] = useState("Seller");
+    const [isEditing, setIsEditing] = useState(false);
+    const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+    const [isDeleteConfirmationOpen, setIsDeleteConfirmationOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        username: "",
+        email: "",
+        description: "",
+    });
+    const [image, setImage] = useState(
+        "https://img.freepik.com/premium-photo/stylish-man-flat-vector-profile-picture-ai-generated_606187-310.jpg"
+    );
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+    const navigate = useNavigate();
+    const fileInputRef = useRef(null);
 
     useEffect(() => {
         axiosInstance
@@ -22,6 +44,131 @@ const SellerProfilePage = () => {
                 console.error("Error fetching Seller:", error);
             });
     }, []);
+    const handleEditProfileSubmit = () => {
+        setIsEditing(true);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSaveChanges = () => {
+        // Check if formData has been changed compared to the original response
+        if (
+            formData.name !== response.name ||
+            formData.username !== response.username ||
+            formData.email !== response.email ||
+            formData.description !== response.description
+        ) {
+            axiosInstance
+                .put("/seller/updateSeller", formData, { withCredentials: true })
+                .then((response) => {
+                    setResponse(response.data);
+                    setIsEditing(false);
+                    alert("Profile updated successfully");
+                })
+                .catch((error) => {
+                    console.error("Error updating profile:", error);
+                });
+        } else {
+            // No changes made; you could also log a message or do nothing
+            setIsEditing(false);
+            console.log("No changes to save");
+        }
+    };
+
+    const handleDeleteAccount = () => {
+        setIsDeleteConfirmationOpen(true);
+    };
+
+    const handleDeleteAccountConfirm = () => {
+        axiosInstance
+            .delete("/seller/deleteSeller", { withCredentials: true })
+            .then(() => {
+                alert("Seller account deleted successfully");
+                Cookies.remove("userType");
+                setUserType("Guest");
+                navigate("/");
+            })
+            .catch((error) => {
+                if (error.response && error.response.status === 400) {
+                    // Show an alert or message indicating that the seller can't be deleted due to pending orders
+                    alert(error.response.data.e); // Display the error message from the backend
+                } else {
+                    console.error("Error deleting Seller account:", error);
+                    alert("An unexpected error occurred while deleting the account.");
+                }
+            });
+    };
+
+    const handleChangePassword = () => {
+        setIsPopUpOpen(true);
+    };
+
+    const handleCurrentPasswordChange = (e) => setCurrentPassword(e.target.value);
+    const handleNewPasswordChange = (e) => setNewPassword(e.target.value);
+    const handleConfirmNewPasswordChange = (e) => setConfirmNewPassword(e.target.value);
+
+    const PopUpAction = () => {
+        if (newPassword !== confirmNewPassword) {
+            alert("New passwords do not match!");
+            return;
+        }
+        axiosInstance
+            .put(
+                "/seller/changeSellerPassword",
+                { oldPassword: currentPassword, newPassword },
+                { withCredentials: true }
+            )
+            .then((response) => {
+                alert("Password changed successfully");
+                setIsPopUpOpen(false);
+                // Clear input fields after submission
+                setCurrentPassword("");
+                setNewPassword("");
+                setConfirmNewPassword("");
+            })
+            .catch((error) => {
+                console.error("Error changing password:", error);
+                alert("Old password is incorrect. Please try again.");
+            });
+    };
+
+    const handleImageClick = () => {
+        fileInputRef.current.click(); // Triggers the file input click
+    };
+
+    // Function to handle file input change
+    const handleImageChange = async (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const formData = new FormData();
+            formData.append("picture", file);
+
+            try {
+                // Send the image to the server
+                const response = await axiosInstance.put(
+                    "/seller/updateSeller",
+                    formData,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                        withCredentials: true,
+                    }
+                );
+
+                // Update state with new image URL from the response
+                setImage(response.data.picture); // Assuming the response returns the new image URL
+
+                alert("Profile picture updated successfully");
+            } catch (error) {
+                console.error("Error updating profile picture:", error);
+                alert("Failed to update profile picture");
+            }
+        }
+    };
 
     return (
         <>
@@ -36,34 +183,78 @@ const SellerProfilePage = () => {
                         backgroundRepeat: "no-repeat",
                     }}
                 ></div>
-                <div
-                    style={{
-                        width: "10vw",
-                        height: "10vw",
-                        borderRadius: "50%",
-                        overflow: "hidden",
-                        border: "4px solid white",
-                        marginTop: "-5vw",
-                        marginLeft: "45%",
-                        backgroundImage:
-                            "url(https://img.freepik.com/premium-photo/stylish-man-flat-vector-profile-picture-ai-generated_606187-310.jpg)",
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                    }}
-                ></div>
                 <div>
-                    {/* Name and Username */}
-                    <h2 style={{ marginTop: "0vw", marginLeft: "45%" }}>
-                        {response?.name || "Seller Name"}
-                    </h2>
-                    <p style={{ color: "gray", marginTop: "-3vw", marginLeft: "47%" }}>
-                        @{response?.username || "username"}
+                    <div
+                        style={{
+                            width: "10vw",
+                            height: "10vw",
+                            borderRadius: "50%",
+                            overflow: "hidden",
+                            border: "4px solid white",
+                            marginTop: "-10vh",
+                            marginLeft: "45%",
+                            backgroundImage: `url(${image})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            cursor: "pointer", // Add cursor pointer to indicate clickability
+                        }}
+                        onClick={handleImageClick} // Add onClick to trigger file input
+                    ></div>
+
+                    {/* Hidden File Input for Profile Image Upload */}
+                    <input
+                        type="file"
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        ref={fileInputRef}
+                        onChange={handleImageChange}
+                    />
+                </div>
+                <div>
+                    {isEditing ? (
+                        <h2
+                            style={{
+                                marginTop: "-1vw",
+                                padding: "1vw",
+                                textAlign: "center",
+                            }}
+                        >
+                            <div>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </h2>
+                    ) : (
+                        <h2
+                            style={{
+                                marginTop: "-1vw",
+                                padding: "1vw",
+                                textAlign: "center",
+                            }}
+                        >
+                            {response?.name || "name not provided"}
+                        </h2>
+                    )}
+                    <p
+                        style={{
+                            color: "gray",
+                            marginTop: "-2.5vh",
+                            textAlign: "center",
+                        }}
+                    >
+                        @{response?.username || "username not provided"}
                     </p>
                     <hr
                         style={{
                             width: "90%",
                             borderTop: "2px solid #ccc",
-                            marginTop: "-1vw",
+                            marginTop: "2vh",
+                            marginLeft: "5vw",
                         }}
                     />
                     <div
@@ -71,27 +262,133 @@ const SellerProfilePage = () => {
                             width: "60%",
                             textAlign: "left",
                             marginTop: "-2vw",
-                            padding: "3vw",
+                            padding: "5vh",
                         }}
                     >
-                        <h3>About Me</h3>
-                        <p>{response?.description || "No description provided"}</p>
+                        <h3 style={{ paddingBottom: "1vh" }}>About Me</h3>
+                        {isEditing ? (
+                            <textarea
+                                name="description"
+                                value={formData.description}
+                                onChange={handleChange}
+                                style={{
+                                    width: "100%",
+                                    height: "150px",
+                                    padding: "10px",
+                                    borderRadius: "5px",
+                                    border: "1px solid #ccc",
+                                    fontSize: "16px",
+                                    resize: "vertical",
+                                }}
+                            />
+                        ) : (
+                            <p>{response?.description || "No description provided"}</p>
+                        )}
 
                         <h3>Profile Details</h3>
-                        <p>
+                        <p style={{ paddingTop: "1vh" }}>
                             <strong>Email:</strong>{" "}
-                            {response?.email || "No email provided"}
+                            {isEditing ? (
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                            ) : (
+                                response?.email || "No email provided"
+                            )}
                         </p>
                     </div>
                 </div>
                 <div>
-                    <ProfileButton />
+                    <ProfileButton
+                        buttonType="changePassword"
+                        onClick={() => handleChangePassword()}
+                    />
+                    <PopUp
+                        isOpen={isPopUpOpen}
+                        setIsOpen={setIsPopUpOpen}
+                        headerText={"Change Password"}
+                        actionText={"Confirm"}
+                        handleSubmit={PopUpAction}
+                    >
+                        <label>Current Password:</label>
+                        <input
+                            type="password"
+                            name="Current Password"
+                            placeholder="Current Password"
+                            onChange={handleCurrentPasswordChange}
+                            value={currentPassword}
+                            style={{
+                                width: "80%", // Full width
+                                padding: "1vw", // Padding for better spacing
+                                marginBottom: "1vw", // Space between inputs
+                                border: "1px solid #ccc", // Border style
+                                borderRadius: "4px", // Rounded corners
+                                alignItems: "center", // Align text to center
+                            }}
+                        />
+                        <label>New Password:</label>
+                        <input
+                            type="password"
+                            name="New Password"
+                            placeholder="Current Password"
+                            onChange={handleNewPasswordChange}
+                            value={newPassword}
+                            style={{
+                                width: "80%", // Full width
+                                padding: "1vw", // Padding for better spacing
+                                marginBottom: "1vw", // Space between inputs
+                                border: "1px solid #ccc", // Border style
+                                borderRadius: "4px", // Rounded corners
+                            }}
+                        />
+                        <label>Confirm New Password:</label>
+                        <input
+                            type="password"
+                            name="Confirm New Password"
+                            placeholder="Current Password"
+                            onChange={handleConfirmNewPasswordChange}
+                            value={confirmNewPassword}
+                            style={{
+                                width: "80%", // Full width
+                                padding: "1vw", // Padding for better spacing
+                                marginBottom: "1vw", // Space between inputs
+                                border: "1px solid #ccc", // Border style
+                                borderRadius: "4px", // Rounded corners
+                            }}
+                        />
+                    </PopUp>
+
+                    <ProfileButton
+                        buttonType="deleteAccount"
+                        onClick={handleDeleteAccount}
+                    />
+                    <PopUp
+                        isOpen={isDeleteConfirmationOpen}
+                        setIsOpen={setIsDeleteConfirmationOpen}
+                        headerText={"Are you sure you want to delete your account?"}
+                        actionText={"Confirm"}
+                        handleSubmit={handleDeleteAccountConfirm}
+                    ></PopUp>
+                    {isEditing ? (
+                        <ProfileButton
+                            buttonType="saveProfile"
+                            onClick={handleSaveChanges}
+                        />
+                    ) : (
+                        <ProfileButton
+                            buttonType="editProfile"
+                            onClick={handleEditProfileSubmit}
+                        />
+                    )}
                 </div>
             </div>
-            <div style={{ position: "fixed", top: 0, left: "9%" }}>
+            <div style={{ position: "fixed", top: 0, left: "9vw" }}>
                 <Navbar />
             </div>
-            <div style={{ position: "fixed", bottom: 0 }}>
+            <div style={{ position: "fixed", bottom: 0, width: "100vw", left: 0 }}>
                 <Footer />
             </div>
         </>
