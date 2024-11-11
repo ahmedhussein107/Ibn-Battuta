7d1722075723a5c89fdbd67a608c2ff2e275070d
abdoo303 <abdelrahimabdelazim303@gmail.com>
2024-11-11 15:40:28 +0200

    added convert currency

---
+++ frontend/src/api/currency.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+const api_key = import.meta.env.VITE_CURRENCY_RATE_API_KEY;
+
+const getExchangeRates = async (baseCurrency = "EGP", targetCurrency = "USD") => {
+    const url = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${baseCurrency}`;
+    try {
+        const response = await axios.get(url);
+        const rate = response.data.conversion_rates[targetCurrency];
+        return rate;
+    } catch (error) {
+        console.error("Error fetching exchange rates:", error);
+    }
+};
+
+const convertCurrency = async (amount, from, to) => {
+    return amount * (await getExchangeRates(from, to));
+};
+export default convertCurrency;
