e2bd20aecc964744d19e8ee3b71df24de5583d63
Ahmed Kamal <105413072+AhmedKamal18@users.noreply.github.com>
2024-12-01 19:03:19 +0200

    Update promocode.router.js

--- backend/routes/promocode.router.js
+++ backend/routes/promocode.router.js
@@ -4,10 +4,12 @@ import {
 	applyPromoCode,
 	createGeneralPromoCode,
 } from "../controllers/promocode.controller.js";
+import { isAuthenticated } from "../routers.middleware/authentication.js";
+
 const promoCodeRouter = express.Router();
 
-promoCodeRouter.post("/validatePromoCode", validatePromoCode);
-promoCodeRouter.post("/applyPromoCode", applyPromoCode);
+promoCodeRouter.post("/validatePromoCode", isAuthenticated, validatePromoCode);
+promoCodeRouter.post("/applyPromoCode", isAuthenticated, applyPromoCode);
 promoCodeRouter.post("/createPromoCode", createGeneralPromoCode);
 
 export default promoCodeRouter;
