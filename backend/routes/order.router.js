beed0aa5f065e040632cf5280601b9292a3aaf96
roba <robahesham25@gmail.com>
2024-12-02 16:28:27 +0200

    modified order to have more than 1 product

--- backend/routes/order.router.js
+++ backend/routes/order.router.js
@@ -2,10 +2,12 @@ import express from "express";
 // import Order from "../models/order.model.js";
 import { isAuthenticated } from "../routers.middleware/authentication.js";
 import {
-	createOrder,
-	getOrders,
-	getOrdersByUser,
-	updateOrder,
+    createOrder,
+    getOrders,
+    getOrdersByUser,
+    updateOrder,
+    completeOrder,
+    deleteOrder,
 } from "../controllers/order.controller.js";
 const orderRouter = express.Router();
 
@@ -14,6 +16,10 @@ orderRouter.post("/createOrder", isAuthenticated, createOrder);
 orderRouter.get("/getOrders", isAuthenticated, getOrders);
 
 orderRouter.patch("/updateOrder/:id", updateOrder);
+
+orderRouter.patch("/completeOrder/:id", completeOrder);
+
+orderRouter.delete("/deleteOrder/:id", deleteOrder);
 
 orderRouter.get("/getMyOrders", isAuthenticated, getOrdersByUser);
 
