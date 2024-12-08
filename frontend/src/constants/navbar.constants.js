53b62cb6a8909f8e2effda18d498b918a86ed290
Ramosa178 <ramosa178@gmail.com>
2024-12-09 00:31:41 +0200

    fixed seller browse

--- frontend/src/constants/navbar.constants.js
+++ frontend/src/constants/navbar.constants.js
@@ -1,75 +1,88 @@
 export const guestNavbarItems = [
-	{ Home: "/" },
-	{
-		Explore: [
-			{ Activities: "/activities" },
-			{ Itineraries: "/itineraries" },
-			{ Landmark: "/landmarks" },
-		],
-	},
-	{ Shop: "/shop" },
+    { Home: "/" },
+    {
+        Explore: [
+            { Activities: "/activities" },
+            { Itineraries: "/itineraries" },
+            { Landmark: "/landmarks" },
+        ],
+    },
+    { Shop: "/shop" },
 ];
 
 export const touristNavbarItems = [
-	{ Home: "/" },
-	{
-		Explore: [
-			{ Activities: "/activities" },
-			{ Itineraries: "/itineraries" },
-			{ Landmark: "/landmarks" },
-		],
-	},
-	{
-		Travel: [{ Flights: "tourist/flight/offers" }, { Hotels: "tourist/hotel/offers" }],
-	},
-	{ Shop: "/shop" },
+    { Home: "/" },
+    {
+        Explore: [
+            { Activities: "/activities" },
+            { Itineraries: "/itineraries" },
+            { Landmark: "/landmarks" },
+        ],
+    },
+    {
+        Travel: [
+            { Flights: "tourist/flight/offers" },
+            { Hotels: "tourist/hotel/offers" },
+        ],
+    },
+    { Shop: "/shop" },
 ];
 
 export const sellerNavbarItems = [
-	{ Home: "/seller" },
-	{ Browse: "seller/shop" },
-	{ Inventory: "seller/inventory" },
-	{ Analytics: "seller/analytics" },
+    { Home: "/seller" },
+    { Browse: "/shop" },
+    { Inventory: "seller/inventory" },
+    { Analytics: "seller/analytics" },
 ];
 
 export const tourGuideNavbarItems = [
-	{ Home: "/tourGuide" },
-	{ Browse: [{ Activites: "/activities" }, { Itineraries: "/itineraries" }] },
-	{ Assigned: "/tourguide/assigned" },
-	{ Analytics: "/tourguide/analytics" },
+    { Home: "/tourGuide" },
+    { Browse: [{ Activites: "/activities" }, { Itineraries: "/itineraries" }] },
+    { Assigned: "/tourguide/assigned" },
+    { Analytics: "/tourguide/analytics" },
 ];
 
 export const advertiserNavbarItems = [
-	{ Home: "/advertiser" },
-	{ Browse: "/activities" },
-	{ Assigned: "/advertiser/assigned" },
-	{ Analytics: "/advertiser/analytics" },
+    { Home: "/advertiser" },
+    { Browse: "/activities" },
+    { Assigned: "/advertiser/assigned" },
+    { Analytics: "/advertiser/analytics" },
 ];
 
 export const governorNavbarItems = [
-	{ Home: "/governor" },
-	{ Browse: "/landmarks" },
-	{ Assigned: [{ Landmarks: "/governor/landmarks" }, { Tags: "/governor/landmarkTags" }] },
+    { Home: "/governor" },
+    { Browse: "/landmarks" },
+    {
+        Assigned: [
+            { Landmarks: "/governor/landmarks" },
+            { Tags: "/governor/landmarkTags" },
+        ],
+    },
 ];
 
 export const adminNavbarItems = [
-	{ Dashboard: "/admin" },
-	{ Inventory: "/admin/inventory" },
-	{
-		"Manage Users": [
-			{ "Users List": "/admin/users" },
-			{ "Pending Users": "/admin/users/pending" },
-			{ Complaints: "/admin/complaints" },
-		],
-	},
-	{
-		Browse: [
-			{ Activities: "/admin/activities" },
-			{ Itineraries: "/admin/itineraries" },
-			{ Landmarks: "/landmarks" },
-			{ Products: "/shop" },
-			{ Analytics: "/admin/analytics" },
-		],
-	},
-	{ Categorization: [{ Tags: "/admin/tags" }, { Category: "/admin/categories" }] },
+    { Dashboard: "/admin" },
+    { Inventory: "/admin/inventory" },
+    {
+        "Manage Users": [
+            { "Users List": "/admin/users" },
+            { "Pending Users": "/admin/users/pending" },
+            { Complaints: "/admin/complaints" },
+        ],
+    },
+    {
+        Browse: [
+            { Activities: "/admin/activities" },
+            { Itineraries: "/admin/itineraries" },
+            { Landmarks: "/landmarks" },
+            { Products: "/shop" },
+            { Analytics: "/admin/analytics" },
+        ],
+    },
+    {
+        Categorization: [
+            { Tags: "/admin/tags" },
+            { Category: "/admin/categories" },
+        ],
+    },
 ];
