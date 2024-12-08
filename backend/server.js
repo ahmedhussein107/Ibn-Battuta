4ab4bca324bc398f11a00a119259801c7fc34007
Ahmed Hussein <ahmedhusseinali107@gmail.com>
2024-12-08 11:55:59 +0200

    added LocationAdder component

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ import amadeusHotelsRouter from "./services/hotels.js";
 import amadeusFlightsRouter from "./services/flights.js";
 import touristCartRouter from "./routes/touristCart.router.js";
 import touristWishlistRouter from "./routes/touristWishlist.router.js";
-// import analyticsRouter from "./routes/analytics.router.js";
+import analyticsRouter from "./routes/analytics.router.js";
 
 import { setupPromoCodeScheduledJobs } from "./controllers/promocode.controller.js";
 
@@ -42,23 +42,23 @@ import { sendNotificationCountToUser, setupWebSocketRoutes } from "./routes/ws.r
 const app = expressWs(express()).app;
 
 connect(MONGO_URI)
-	.then(() => {
-		app.listen(PORT, () => {
-			console.log(`Connected to DB`);
-			console.log(`Listening to port ${PORT}`);
-		});
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Connected to DB`);
+            console.log(`Listening to port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 setupWebSocketRoutes(app);
 setupPromoCodeScheduledJobs();
 
 app.use(cookieParser());
 const corsOptions = {
-	origin: "http://localhost:5173",
-	credentials: true,
+    origin: "http://localhost:5173",
+    credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -93,4 +93,4 @@ app.use("/api/bookmark", touristBookmarkRouter);
 app.use("/api/general", generalRouter);
 app.use("/api/cart", touristCartRouter);
 app.use("/api/wishlist", touristWishlistRouter);
-// app.use("/api/analytics", analyticsRouter);
+app.use("/api/analytics", analyticsRouter);
