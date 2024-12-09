61ad54fee74630cfb2d00c34436cc15b3df438b1
Ahmed Hussein <ahmedhusseinali107@gmail.com>
2024-12-09 04:27:57 +0200

    fixed my landmarks route

--- backend/controllers/landmark.controller.js
+++ backend/controllers/landmark.controller.js
@@ -3,97 +3,97 @@ import Landmark from "../models/landmark.model.js";
 import { genericSearch, buildFilter } from "../utilities/searchUtils.js";
 
 export const createLandmark = async (req, res) => {
-	try {
-		const landmark = await Landmark.create(req.body);
-		res.status(201).json(landmark);
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+    try {
+        const landmark = await Landmark.create(req.body);
+        res.status(201).json(landmark);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const getAllLandmarks = async (req, res) => {
-	try {
-		const { page, limit, ...rest } = buildFilter(req.query);
-		const query = buildFilter(rest);
-		const _page = Math.max(1, parseInt(req.query.page) || 1);
-		const _limit = Math.max(1, parseInt(req.query.limit) || 10);
-		const toSkip = (_page - 1) * _limit;
-		let landmarks = await Landmark.find(query);
+    try {
+        const { page, limit, ...rest } = buildFilter(req.query);
+        const query = buildFilter(rest);
+        const _page = Math.max(1, parseInt(req.query.page) || 1);
+        const _limit = Math.max(1, parseInt(req.query.limit) || 10);
+        const toSkip = (_page - 1) * _limit;
+        let landmarks = await Landmark.find(query);
 
-		const count = landmarks.length;
-		landmarks = landmarks.slice(toSkip, Math.min(toSkip + _limit, count));
-		return res.status(200).json({
-			result: landmarks,
-			totalPages: count > 0 ? Math.ceil(count / _limit) : 1,
-		});
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+        const count = landmarks.length;
+        landmarks = landmarks.slice(toSkip, Math.min(toSkip + _limit, count));
+        return res.status(200).json({
+            result: landmarks,
+            totalPages: count > 0 ? Math.ceil(count / _limit) : 1,
+        });
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const updateLandmark = async (req, res) => {
-	try {
-		console.log(req.body);
-		const landmark = await Landmark.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-		});
+    try {
+        console.log(req.body);
+        const landmark = await Landmark.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
 
-		// console.log(landmark);
+        // console.log(landmark);
 
-		if (!landmark) {
-			return res.status(404).json({ error: "Landmark not found" });
-		}
+        if (!landmark) {
+            return res.status(404).json({ error: "Landmark not found" });
+        }
 
-		res.status(200).json(landmark);
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+        res.status(200).json(landmark);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const getTicketPricesFromLandmark = async (req, res) => {
-	try {
-		const landmark = await Landmark.findById(req.params.id);
-		if (!landmark) {
-			return res.status(404).json({ error: "Landmark not found" });
-		}
-		res.status(200).json(landmark.ticketPrices);
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+    try {
+        const landmark = await Landmark.findById(req.params.id);
+        if (!landmark) {
+            return res.status(404).json({ error: "Landmark not found" });
+        }
+        res.status(200).json(landmark.ticketPrices);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const getLandmarkById = async (req, res) => {
-	try {
-		const landmark = await Landmark.findById(req.params.id);
-		if (!landmark) {
-			return res.status(404).json({ error: "Landmark not found" });
-		}
+    try {
+        const landmark = await Landmark.findById(req.params.id);
+        if (!landmark) {
+            return res.status(404).json({ error: "Landmark not found" });
+        }
 
-		res.status(200).json(landmark);
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+        res.status(200).json(landmark);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const deleteLandmark = async (req, res) => {
-	try {
-		const landmark = await Landmark.findByIdAndDelete(req.params.id);
-		if (!landmark) {
-			return res.status(404).json({ error: "Landmark not found" });
-		}
-		res.status(200).json(landmark);
-	} catch (e) {
-		res.status(400).json({ error: e.message });
-	}
+    try {
+        const landmark = await Landmark.findByIdAndDelete(req.params.id);
+        if (!landmark) {
+            return res.status(404).json({ error: "Landmark not found" });
+        }
+        res.status(200).json(landmark);
+    } catch (e) {
+        res.status(400).json({ error: e.message });
+    }
 };
 
 export const getGovernorLandmarks = async (req, res) => {
-	const governorId = req.params.id;
-	try {
-		const landmarks = await Landmark.find({ governorID: governorId });
-		res.status(200).json(landmarks);
-	} catch (error) {
-		console.error("Error fetching landmarks:", error);
-		res.status(500).json({ message: "Internal Server Error" });
-	}
+    const governorId = req.user.userId;
+    try {
+        const landmarks = await Landmark.find({ governorID: governorId });
+        res.status(200).json(landmarks);
+    } catch (error) {
+        console.error("Error fetching landmarks:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
--- frontend/src/pages/Governor/GovernorLandmarks.jsx
+++ frontend/src/pages/Governor/GovernorLandmarks.jsx
@@ -12,123 +12,123 @@ import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
 
 const governorLandmark = () => {
-	const [landmarks, setLandmarks] = useState([]);
+    const [landmarks, setLandmarks] = useState([]);
 
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(1);
-	const itemsPerPage = 6;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const itemsPerPage = 6;
 
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	const fetchLandmarks = async () => {
-		try {
-			const response = await axiosInstance.get(`/landmark/allLandmarks/`, {
-				params: {
-					page: currentPage,
-					limit: itemsPerPage,
-				},
-			});
-			console.log("response", response.data);
-			setTotalPages(response.data.totalPages);
-			setLandmarks(response.data.result);
-		} catch (error) {
-			console.error("Error fetching landmarks:", error);
-		}
-	};
+    const fetchLandmarks = async () => {
+        try {
+            const response = await axiosInstance.get(`/landmark/getGovernorLandmarks/`, {
+                params: {
+                    page: currentPage,
+                    limit: itemsPerPage,
+                },
+                withCredentials: true,
+            });
+            console.log("response", response.data);
+            setLandmarks(response.data);
+        } catch (error) {
+            console.error("Error fetching landmarks:", error);
+        }
+    };
 
-	useEffect(() => {
-		fetchLandmarks();
-	}, [currentPage]);
+    useEffect(() => {
+        fetchLandmarks();
+    }, [currentPage]);
 
-	return (
-		<div style={{ width: "100vw", position: "absolute", top: "0", left: "0" }}>
-			<div
-				style={{
-					width: "100vw",
-					height: "35vh",
-					color: "#FAE2B6",
-					backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${landmarkbackground})`,
-					backgroundSize: "100% 100%",
-					backgroundPosition: "center",
-					backgroundRepeat: "no-repeat",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "flex-end",
-					alignItems: "center",
-				}}
-			>
-				<div style={{ marginLeft: "5%", marginBottom: "2%" }}>
-					<p
-						style={{
-							fontSize: "2.5rem",
-							marginBottom: "1rem",
-							textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-							color: "white",
-							fontWeight: "500",
-							userSelect: "none",
-						}}
-					>
-						My LandMarks
-					</p>
-				</div>
-			</div>
+    return (
+        <div style={{ width: "100vw", position: "absolute", top: "0", left: "0" }}>
+            <div
+                style={{
+                    width: "100vw",
+                    height: "35vh",
+                    color: "#FAE2B6",
+                    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${landmarkbackground})`,
+                    backgroundSize: "100% 100%",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                }}
+            >
+                <div style={{ marginLeft: "5%", marginBottom: "2%" }}>
+                    <p
+                        style={{
+                            fontSize: "2.5rem",
+                            marginBottom: "1rem",
+                            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                            color: "white",
+                            fontWeight: "500",
+                            userSelect: "none",
+                        }}
+                    >
+                        My LandMarks
+                    </p>
+                </div>
+            </div>
 
-			<Button
-				style={{
-					marginTop: "1vh",
-					marginLeft: "2vw",
-					borderRadius: "4vh",
-					minWidth: "1vw",
-					color: "black",
-					borderColor: "black",
-					maxHeight: "4.2vh",
-				}}
-				variant="outlined"
-				onClick={() => {
-					navigate("/governor/create-landmark");
-				}}
-			>
-				<AddIcon sx={{ fontSize: "3vh" }} />
-				<p style={{ marginLeft: ".3vw" }}>Add Landmark</p>
-			</Button>
+            <Button
+                style={{
+                    marginTop: "1vh",
+                    marginLeft: "2vw",
+                    borderRadius: "4vh",
+                    minWidth: "1vw",
+                    color: "black",
+                    borderColor: "black",
+                    maxHeight: "4.2vh",
+                }}
+                variant="outlined"
+                onClick={() => {
+                    navigate("/governor/create-landmark");
+                }}
+            >
+                <AddIcon sx={{ fontSize: "3vh" }} />
+                <p style={{ marginLeft: ".3vw" }}>Add Landmark</p>
+            </Button>
 
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "row",
-					marginLeft: "2%",
-					marginTop: "1%",
-					marginBottom: "1%",
-					justifyContent: "center",
-				}}
-			>
-				<div style={{ width: "100vw" }}>
-					{landmarks.map((landmark) => {
-						return (
-							<div style={{ marginBottom: "3vh" }}>
-								<CardLandmark
-									landmark={landmark}
-									width={"95%"}
-									height={"40vh"}
-									firstLineButtons={[<DeleteButton />]}
-								/>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-			<div style={{ paddingBottom: "1%" }}>
-				<PaginationComponent
-					totalPages={totalPages}
-					currentPage={currentPage}
-					onChange={(event, newPage) => {
-						setCurrentPage(newPage);
-					}}
-				/>
-			</div>
-			<Footer />
-		</div>
-	);
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginLeft: "2%",
+                    marginTop: "1%",
+                    marginBottom: "1%",
+                    justifyContent: "center",
+                }}
+            >
+                <div style={{ width: "100vw" }}>
+                    {landmarks.map((landmark) => {
+                        return (
+                            <div style={{ marginBottom: "3vh" }}>
+                                <CardLandmark
+                                    landmark={landmark}
+                                    width={"95%"}
+                                    height={"40vh"}
+                                    firstLineButtons={[<DeleteButton />]}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div style={{ paddingBottom: "1%" }}>
+                <PaginationComponent
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onChange={(event, newPage) => {
+                        setCurrentPage(newPage);
+                    }}
+                />
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default governorLandmark;
