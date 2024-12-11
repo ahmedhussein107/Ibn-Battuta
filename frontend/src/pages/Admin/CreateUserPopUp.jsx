8d4ea93ea1a04df776d30cbe0f05645dac189e4b
roba <robahesham25@gmail.com>
2024-12-11 15:55:30 +0200

    fixed popup and added feedback in adding new user

--- frontend/src/pages/Admin/CreateUserPopUp.jsx
+++ frontend/src/pages/Admin/CreateUserPopUp.jsx
@@ -3,11 +3,18 @@ import PopUp from "../../components/PopUpsGeneric/PopUp";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import axiosInstance from "../../api/axiosInstance";
+import Alert from "@mui/material/Alert";
 import { useState } from "react";
 import "./UserManagement.css";
 const CreateUserPopUp = ({ userType, isOpen, setIsOpen }) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [popupAlert, setPopupAlert] = useState({
+        open: false,
+        severity: "info",
+        message: "",
+    });
+
     const handleSubmit = async () => {
         try {
             await axiosInstance.post(`${userType.toLowerCase()}/create${userType}`, {
@@ -15,12 +22,22 @@ const CreateUserPopUp = ({ userType, isOpen, setIsOpen }) => {
                 name: username,
                 password,
             });
+            showPopUpAlert("success", "User created successfully");
         } catch (err) {
             console.log(err);
-            setError(err.response.data.e ? err.response.data.e : err.response.data);
+            showPopUpAlert("error", err.response.data.message);
         } finally {
-            setIsOpen(false);
+            //setIsOpen(false);
         }
+    };
+
+    const showPopUpAlert = (severity, message) => {
+        setPopupAlert({ open: true, severity, message });
+
+        setTimeout(() => {
+            setPopupAlert({ open: false, severity: "", message: "" }); // Close the alert after some time
+            setIsOpen(false);
+        }, 4000); // Alert will close after 5 seconds
     };
     return (
         <PopUp
@@ -31,17 +48,82 @@ const CreateUserPopUp = ({ userType, isOpen, setIsOpen }) => {
             actionText="Create"
             handleSubmit={handleSubmit}
         >
-            <div className="create-user-input-container">
-                <input
+            <div>
+                <p style={{ fontWeight: "bold" }}>Username</p>
+                <Box
+                    component="form"
+                    sx={{
+                        "& > :not(style)": { m: 1, width: "25ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="outlined-required"
+                        label="Username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        style={{
+                            width: "25vw",
+                            height: "4vh",
+                            marginTop: "1vh",
+                            marginLeft: "0vw",
+                            marginBottom: "2vh",
+                        }}
+                    />
+                </Box>
+                {/* <input
                     type="text"
                     placeholder="Enter the username"
                     onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
+                /> */}
+                <p style={{ fontWeight: "bold" }}>Password</p>
+                <Box
+                    component="form"
+                    sx={{
+                        "& > :not(style)": { m: 1, width: "25ch" },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    <TextField
+                        id="outlined-required"
+                        label="Password"
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        style={{
+                            width: "25vw",
+                            height: "4vh",
+                            marginTop: "1vh",
+                            marginLeft: "0vw",
+                            marginBottom: "2vh",
+                        }}
+                    />
+                </Box>
+                {popupAlert.open && (
+                    <Alert
+                        severity={popupAlert.severity}
+                        onClose={() =>
+                            setPopupAlert({
+                                ...popupAlert,
+                                open: false,
+                            })
+                        }
+                        style={{
+                            marginBottom: "1vh",
+                            fontSize: "22px",
+                            textAlign: "center",
+                            marginTop: "2vh",
+                        }}
+                    >
+                        {popupAlert.message}
+                    </Alert>
+                )}
+
+                {/* <input
                     type="password"
                     placeholder="Enter the password"
                     onChange={(e) => setPassword(e.target.value)}
-                />
+                /> */}
             </div>
         </PopUp>
     );
